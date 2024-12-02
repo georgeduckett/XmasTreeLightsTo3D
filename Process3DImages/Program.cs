25e813a95e832d2d6e51953bc22b4427331f3adf
George Duckett <georgeduckett@gmail.com>
2024-12-02 23:56:15 +0000

    Initial translation of process images

--- Process3DImages/Program.cs
+++ Process3DImages/Program.cs
@@ -1,4 +1,4 @@
-﻿using MathNet.Symbolics;
+﻿using HSG.Numerics;
 using OpenCvSharp;
 
 var LEDCount = 400;
@@ -9,74 +9,44 @@ var blurAmount = (double)41;
 
 MinMaxLocResult ImageBP(string filePath)
 {
-    var image = Cv2.ImRead(filePath);
-    var orig = new Mat();
+    using var image = Cv2.ImRead(filePath);
+    using var orig = new Mat();
     image.CopyTo(orig);
-    var gray = new Mat();
+    using var gray = new Mat();
     Cv2.CvtColor(orig, gray, ColorConversionCodes.BGR2GRAY);
     Cv2.MinMaxLoc(gray, out var minVal, out var maxVal, out var minLoc, out var maxLoc);
     return new MinMaxLocResult(minVal, maxVal, minLoc, maxLoc);
 }
 
 
-var ImageBasePath = "C:\\Users\\";
+var ImageBasePath = "C:\\Users\\Lucy Duckett\\Source\\repos\\XmasTree\\XmasTree\\01_calibration";
 
 // Set all paths
-foreach(var point in Points)
+foreach (var point in Points)
 {
-    point.image_1_path = Path.Join(ImageBasePath, $"{point.index}_0.png");
-    point.image_2_path = Path.Join(ImageBasePath, $"{point.index}_45.png");
-    point.image_3_path = Path.Join(ImageBasePath, $"{point.index}_90.png");
-    point.image_4_path = Path.Join(ImageBasePath, $"{point.index}_135.png");
-    point.image_5_path = Path.Join(ImageBasePath, $"{point.index}_180.png");
-    point.image_6_path = Path.Join(ImageBasePath, $"{point.index}_225.png");
-    point.image_7_path = Path.Join(ImageBasePath, $"{point.index}_270.png");
-    point.image_8_path = Path.Join(ImageBasePath, $"{point.index}_315.png");
+    for (var i = 0; i < point.imagepath.Length; i++)
+    {
+        point.imagepath[i] = Path.Join(ImageBasePath, $"{point.index}_{i * 45}.png");
+    }
 }
 
 // Find brightest points
 foreach(var point in Points)
 {
-    var minMax1 = ImageBP(point.image_1_path);
-    var minMax2 = ImageBP(point.image_2_path);
-    var minMax3 = ImageBP(point.image_3_path);
-    var minMax4 = ImageBP(point.image_4_path);
-    var minMax5 = ImageBP(point.image_5_path);
-    var minMax6 = ImageBP(point.image_6_path);
-    var minMax7 = ImageBP(point.image_7_path);
-    var minMax8 = ImageBP(point.image_8_path);
-
-    point.i1x = minMax1.MaxLoc.X;
-    point.i2x = minMax2.MaxLoc.X;
-    point.i3x = minMax3.MaxLoc.X;
-    point.i4x = minMax4.MaxLoc.X;
-    point.i5x = minMax5.MaxLoc.X;
-    point.i6x = minMax6.MaxLoc.X;
-    point.i7x = minMax7.MaxLoc.X;
-    point.i8x = minMax8.MaxLoc.X;
-
-    point.i1y = minMax1.MaxLoc.Y;
-    point.i2y = minMax2.MaxLoc.Y;
-    point.i3y = minMax3.MaxLoc.Y;
-    point.i4y = minMax4.MaxLoc.Y;
-    point.i5y = minMax5.MaxLoc.Y;
-    point.i6y = minMax6.MaxLoc.Y;
-    point.i7y = minMax7.MaxLoc.Y;
-    point.i8y = minMax8.MaxLoc.Y;
-
-    point.i1w = minMax1.MaxVal;
-    point.i2w = minMax2.MaxVal;
-    point.i3w = minMax3.MaxVal;
-    point.i4w = minMax4.MaxVal;
-    point.i5w = minMax5.MaxVal;
-    point.i6w = minMax6.MaxVal;
-    point.i7w = minMax7.MaxVal;
-    point.i8w = minMax8.MaxVal;
+    Console.Write($"\rFind brightest points. {point.index} of {Points.Length}");
+    for (var i = 0; i < point.imagepath.Length; i++)
+    {
+        var minMax = ImageBP(point.imagepath[i]);
+        point.ix[i] = minMax.MaxLoc.X;
+        point.iy[i] = minMax.MaxLoc.Y;
+        point.iw[i] = minMax.MaxVal;
+    }
 }
 
 // Find the average Ys
 foreach(var point in Points)
 {
+    Console.Write($"\nSet average Ys. {point.index} of {Points.Length}");
     point.actualy = point.averageYs();
 }
 
@@ -90,14 +60,10 @@ var average_x = average_xs.Sum() / (average_xs.Length * 8); // We multiply by 8 
 // Adjust all x values
 foreach(var point in Points)
 {
-    point.i1x -= average_x;
-    point.i2x -= average_x;
-    point.i3x -= average_x;
-    point.i4x -= average_x;
-    point.i5x -= average_x;
-    point.i6x -= average_x;
-    point.i7x -= average_x;
-    point.i8x -= average_x;
+    for(int i = 0; i < point.ix.Length; i++)
+    {
+        point.ix[i] -= average_x;
+    }
 }
 
 IEnumerable<(T First, T Second)> CombinationsOfTwo<T>(T[] list)
@@ -111,85 +77,110 @@ IEnumerable<(T First, T Second)> CombinationsOfTwo<T>(T[] list)
     }
 }
 
+// Solve the equations to go from x position on image and tree rotation, an r and theta (radius and angle?)
 foreach (var point in Points)
 {
-    var r = Symbol.NewSymbol("r");
-    var theta = Symbol.NewSymbol("theta");
+    Console.Write($"\rSolve equations. {point.index} of {Points.Length}");
+    var equ_Permutations = CombinationsOfTwo(point.eqn).ToArray();
+    var weights_Permutations = CombinationsOfTwo(point.iw).ToArray();
 
-    point.eqn1 = new SymbolicExpression(r )
+    var rValues = new List<double>(equ_Permutations.Length);
+    var thetaValues = new List<double>(equ_Permutations.Length);
+    var weightValues = new List<double>(equ_Permutations.Length);
+
+    for (int i = 0; i < equ_Permutations.Length; i++)
+    {
+        // Use HSG.Numerics to solve the system of (2) non-linear equations
+
+        // This functions takes in the variables (in r, theta order) and runs the two equations and returns an array with the two results
+        double[] FuncsToSolve(double[] variables)
+        {
+            var result = new double[2]; // There are two equations
+
+            result[0] = equ_Permutations[i].First(variables[0], variables[1]);
+            result[1] = equ_Permutations[i].Second(variables[0], variables[1]);
+            return result;
+        }
+
+        (var solutions, var bestEquationResults, var info) = Fsolve.Fsolver(FuncsToSolve, 2, [0.0, 0.0], 1e-10);
+
+        rValues.Add(solutions[0]);
+        thetaValues.Add(solutions[1]);
+        weightValues.Add(weights_Permutations[i].First * weights_Permutations[i].Second);
+    }
+
+    var values = Enumerable.Zip(rValues, thetaValues, weightValues);
+
+    var rSum = values.Sum(values => values.First * values.Third * values.Third);
+    var thetaSum = values.Sum(values => values.Second * values.Third * values.Third);
+    var wSum = values.Sum(values => values.Third * values.Third);
+
+    point.r = rSum / (wSum + 0.000000001);
+    point.theta = thetaSum / (wSum + 0.000000001);
+}
+
+
+
+// Now write the csv
+File.WriteAllLines("coordinates.csv", new[] { "index, x, y, z, r, theta" }
+    .Concat(Points.Select(p => $"{p.index}, {p.r * Math.Cos(p.theta!.Value)}, {p.r * Math.Sin(p.theta!.Value)}, {p.actualy}, {p.r}, {p.theta}")));
+foreach(var point in Points)
+{
+    var index = point.index;
+    var x = point.r * Math.Cos(point.theta!.Value);
+    var y = point.r * Math.Sin(point.theta!.Value);
+    var z = point.actualy;
+    var r = point.r;
+    var theta = point.theta!.Value;
 }
 
 public record MinMaxLocResult(double MinVal, double MaxVal, OpenCvSharp.Point MinLoc, OpenCvSharp.Point MaxLoc);
-public class Point(int i)
+public class Point
 {
-    public int index = i;
+    public int index;
     public double? r;
     public double? theta;
-    public string? image_1_path;
-    public string? image_2_path;
-    public string? image_3_path;
-    public string? image_4_path;
-    public string? image_5_path;
-    public string? image_6_path;
-    public string? image_7_path;
-    public string? image_8_path;
+    public string[] imagepath = new string[8];
 
     public double? actualy;
 
     // the positions to the x values
-    public double? i1x;
-    public double? i2x;
-    public double? i3x;
-    public double? i4x;
-    public double? i5x;
-    public double? i6x;
-    public double? i7x;
-    public double? i8x;
+    public double[] ix = new double[8];
     // the positon to the y values
-    public double? i1y;
-    public double? i2y;
-    public double? i3y;
-    public double? i4y;
-    public double? i5y;
-    public double? i6y;
-    public double? i7y;
-    public double? i8y;
+    public double[] iy = new double[8];
     // the the weights
-    public double? i1w;
-    public double? i2w;
-    public double? i3w;
-    public double? i4w;
-    public double? i5w;
-    public double? i6w;
-    public double? i7w;
-    public double? i8w;
+    public double[] iw = new double[8];
+    public Func<double, double, double>[] eqn = new Func<double, double, double>[8];
 
-    // equations
-    public double? eqn1;
-    public double? eqn2;
-    public double? eqn3;
-    public double? eqn4;
-    public double? eqn5;
-    public double? eqn6;
-    public double? eqn7;
-    public double? eqn8;
+    public Point(int i)
+    {
+        index = i;
+
+        eqn[0] = (r, theta) => r * Math.Cos(theta + 0 * Math.PI / 4) - ix[0];
+        eqn[1] = (r, theta) => r * Math.Cos(theta + 1 * Math.PI / 4) - ix[1];
+        eqn[2] = (r, theta) => r * Math.Cos(theta + 2 * Math.PI / 4) - ix[2];
+        eqn[3] = (r, theta) => r * Math.Cos(theta + 3 * Math.PI / 4) - ix[3];
+        eqn[4] = (r, theta) => r * Math.Cos(theta + 4 * Math.PI / 4) - ix[4];
+        eqn[5] = (r, theta) => r * Math.Cos(theta + 5 * Math.PI / 4) - ix[5];
+        eqn[6] = (r, theta) => r * Math.Cos(theta + 6 * Math.PI / 4) - ix[6];
+        eqn[7] = (r, theta) => r * Math.Cos(theta + 7 * Math.PI / 4) - ix[7];
+    }
+
 
     public override string ToString()
     {
-        return $"the x values are: {i1x}, {i2x}, {i3x}, {i4x}, {i5x}, {i6x}, {i7x}, {i8x}, y values are: {i1y}, {i2y}, {i3y}, {i4y}, {i5y}, {i6y}, {i7y}, {i8y}, the weights are: {i1w}, {i2w}, {i3w}, {i4w}, {i5w}, {i6w}, {i7w}, {i8w}";
+        return $"the x values are: {string.Join(", ", ix)}, y values are: {string.Join(", ", iy)}, the weights are: {string.Join(", ", iw)}";
     }
 
     public double averageYs()
     {
-        var y_s = new[] { i1y * i1w, i2y * i2w, i3y * i3w, i4y * i4w, i5y * i5w, i6y * i6w, i7y * i7w, i8y * i8w };
-        var weights = new[] { i1w, i2w, i3w, i4w, i5w, i6w, i7w, i8w }; // TODO: Should have i8w?
-        return (y_s.Sum() / weights.Sum()).Value; // TODO: Shouldn't we also divide by the number of them?? (8)
+        var y_s = new[] { iy[0] * iw[0], iy[1] * iw[1], iy[2] * iw[2], iy[3] * iw[3], iy[4] * iw[4], iy[5] * iw[5], iy[6] * iw[6], iy[7] * iw[7] };
+        return y_s.Sum() / iw.Sum(); // TODO: Shouldn't we also divide by the number of them?? (8) We don't as we do that when getting average
     }
 
     public double averageXs()
     {
-        var x_s = new[] { i1x * i1w, i2x * i2w, i3x * i3w, i4x * i4w, i5x * i5w, i6x * i6w, i7x * i7w, i8x * i8w };
-        var weights = new[] { i1w, i2w, i3w, i4w, i5w, i6w, i7w, i8w }; // TODO: Shouldn't we also divide by the number of them?? (8) We don't as we do that when getting average
-        return (x_s.Sum() / weights.Sum()).Value;
+        var x_s = new[] { ix[0] * iw[0], ix[1] * iw[1], ix[2] * iw[2], ix[3] * iw[3], ix[4] * iw[4], ix[5] * iw[5], ix[6] * iw[6], ix[7] * iw[7] };
+        return x_s.Sum() / iw.Sum(); // TODO: Shouldn't we also divide by the number of them?? (8) We don't as we do that when getting average
     }
 }
