09194aff5a6504efb00203c1bb244e635159d317
George Duckett <georgeduckett@gmail.com>
2025-01-24 19:07:36 +0000

    Create page and controller, and first starting bits for image processing

---
+++ TreeLightsWeb/Controllers/ImageProcessingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using TreeLightsWeb.BackgroundTaskManagement;
+using TreeLightsWeb.Models;
+
+namespace TreeLightsWeb.Controllers
+{
+    public class ImageProcessingController : Controller
+    {
+        private readonly ILogger<ImageCaptureController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ITreeTaskManager _treeTaskManager;
+        private readonly IHubContext<TreeHub> _TreeHubContext;
+
+        public ImageProcessingController(ILogger<ImageCaptureController> logger, IWebHostEnvironment webHostEnvironment, ITreeTaskManager treeTaskManager, IHubContext<TreeHub> treeHubContext)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+            _treeTaskManager = treeTaskManager;
+            _TreeHubContext = treeHubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartImageProcessing(string connectionId, [FromBody] ImageProcessingModel model)
+        {
+            await _TreeHubContext.Clients.Client(connectionId).SendAsync("UpdateProcessingProgress", "Started");
+            await Task.Delay(5000);
+            await _TreeHubContext.Clients.Client(connectionId).SendAsync("UpdateProcessingProgress", "Finished!");
+
+            return Ok();
+        }
+    }
+}
---
+++ TreeLightsWeb/Models/ImageProcessingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TreeLightsWeb.Models
+{
+    public class ImageProcessingModel
+    {
+        public double BlurAmount { get; set; }
+        public double BrightnessThreshold { get; set; }
+        public int MinPointsToKeep { get; set; }
+        public int MaxYDiffBetweenTreeRotations { get; set; }
+        public double ProportionToAssumeCorrectDistances { get; set; }
+    }
+}
