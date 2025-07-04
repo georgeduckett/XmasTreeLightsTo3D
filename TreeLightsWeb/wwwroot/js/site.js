6a8e2a100f81a612499e2a1d678e990e9a5f6d4f
George Duckett <georgeduckett@gmail.com>
2025-07-04 19:02:26 +0100

    Show reconnection link. poll tree until connected

--- TreeLightsWeb/wwwroot/js/site.js
+++ TreeLightsWeb/wwwroot/js/site.js
@@ -1,5 +1,14 @@
-﻿$.get('/Home/IsTreeConnected', function (data) {
-    if (data === "False") {
-        $('.treenotconnectednav').show();
-    }
+﻿function checkTreeConnection() {
+    $.get('/Home/IsTreeConnected', function (data) {
+        if (data === "False") {
+            $('.treenotconnectednav').show();
+            setTimeout(checkTreeConnection, 5000);
+        } else {
+            $('.treenotconnectednav').hide();
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    checkTreeConnection();
 });
