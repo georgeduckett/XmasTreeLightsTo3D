ce5e3a0ca202bd3aecc87f2f819a338eed4b2e20
George Duckett <georgeduckett@gmail.com>
2025-12-10 14:08:38 +0000

    Add stranger things project

---
+++ StrangerThingsLights/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StrangerThingsLights.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
