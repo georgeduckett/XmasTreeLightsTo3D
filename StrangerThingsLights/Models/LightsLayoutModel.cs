b4755ef57d28c84ea83f8a9cfff5d47337da4e0a
George Duckett <georgeduckett@gmail.com>
2025-12-14 12:17:13 +0000

    Handle reverse letter sequences

--- StrangerThingsLights/Models/LightsLayoutModel.cs
+++ StrangerThingsLights/Models/LightsLayoutModel.cs
@@ -2,29 +2,26 @@
 {
     public class LightsLayoutModel
     {
-        public record LetterMapping(char Letter, ushort Position);
+        public record LetterMapping(char StartLetter, ushort StartPosition, char EndLetter, int IndexIncrement);
         public LetterMapping[] LetterMappings { get; set; } = [];
 
         private int[] LetterIndexes = new int[26];
 
         public void Validate()
         {
-            // TODO: Handle some letter sequences going backwards
             foreach (var mapping in LetterMappings)
             {
-                if (mapping.Letter < 'a' || mapping.Letter > 'z')
-                {
-                    throw new ArgumentOutOfRangeException(nameof(LetterMappings), $"Letter '{mapping.Letter}' is out of range. Must be between 'a' and 'z'.");
-                }
-                LetterIndexes[mapping.Letter - 'a'] = mapping.Position;
-            }
+                var count = Math.Abs(mapping.EndLetter - mapping.StartLetter) + 1;
+                var letterIncrement = mapping.EndLetter >= mapping.StartLetter ? 1 : -1;
 
-            for (var letter = 'a'; letter <= 'z'; letter++)
-            {
-                if (!LetterMappings.Any(m => m.Letter == letter))
+                for (var i = 0; i < count; i++)
                 {
-                    // We don't have a letter mapping for this one, so assume it's sequential (see what index the previous letter had and add 1)
-                    LetterIndexes[letter - 'a'] = LetterIndexes[letter - 'a' - 1] + 1;
+                    var letter = (char)(mapping.StartLetter + i * letterIncrement);
+                    if (letter < 'a' || letter > 'z')
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(LetterMappings), $"Letter '{letter}' is out of range. Must be between 'a' and 'z'. Mapping with start letter {mapping.StartLetter}.");
+                    }
+                    LetterIndexes[letter - 'a'] = mapping.StartPosition + i * (mapping.IndexIncrement == 0 ? 1 : mapping.IndexIncrement);
                 }
             }
         }
